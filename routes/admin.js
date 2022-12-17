0fc5ea9ba1e7fa8ba8c9d4eb31e9793df8fabae5
Almir Hodžić <hdzcalmir@gmail.com>
2022-12-17 20:05:30 +0100

    forma kreiraj

--- routes/admin.js
+++ routes/admin.js
@@ -4,4 +4,8 @@ const router = express.Router();
 // stavljamo samo kosu crtu jer se podrazumijeva da je admin, jer se nalazimo u tom fajlu
 router.get('/', require('../controllers/admin/adminController'));
 
+router.get('/create', (req, res) => {
+    res.render('admin/adminCreateForm')
+});
+
 module.exports = router;
